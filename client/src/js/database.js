3cd96b68b9f3559b491fa33967472e357d921dda
RickiBobbii <rickibobbiii@gmail.com>
2024-01-14 17:28:32 -0500

    fixed putDb issue

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,11 +19,10 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  //issue with content
-  const request = store.put({ jate: content});
+  //issue with content needs to be an object {content}
+  const request = store.put({ content });
   const result = await request;
   console.log('Data saved to database', result);
-  //return result;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -35,7 +34,6 @@ export const getDb = async () => {
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
-  //return result;
 };
 
 initdb();
